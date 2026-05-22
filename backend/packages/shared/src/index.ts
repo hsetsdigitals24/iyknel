871b42f6550e38803b171bbdaa4caf7814f10afd
hsetsdigitals24 <hsets.com@gmail.com>
2026-05-22 13:54:48 +0100

    Delete backend directory

--- backend/packages/shared/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all types from types module
-export * from './types';
-
-// Export auth utilities
-export * from './auth';
-
-// Export cache utilities
-export { CacheService } from './cache';
